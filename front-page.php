7ffc9526a188e6734bc54d7a89a6098b90243f65
Ryo Fujiwara <jjcproject@icloud.com>
2022-07-15 18:50:08 +0900

    upload

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Template Name: Front Page
@@ -10,64 +10,78 @@ get_header() ?>
     <div class="container-fluid p-0">
         <img src="<?php echo get_template_directory_uri(); ?>/image/img001-tokio.png" alt="" class="img-fluid">
         <div class="logo-area">
-        <h1>TOKIO INKARAMI</h1>
-        <p>TOKIOインカラミ公式サイト</p>
+            <h1>TOKIO INKARAMI</h1>
+            <p>TOKIOインカラミ公式サイト</p>
         </div>
     </div>
 </header>
 
 <section class="top-contents my-5">
+<div class="container d-flex justify-content-center">
+
+<div class="col-lg-6 col-md-6 d-flex justify-content-center my-4">
+    <?php
+
+    if (has_post_thumbnail()) { ?>
+        <img class="img-fluid" src="<?php echo get_the_post_thumbnail_url() ?>" alt="<? echo get_the_title() ?>">
+    <?php } else { ?>
+        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/image/placeholder.png" alt="<? echo get_the_title() ?>">
+    <?php }
+
+    ?>
+</div>
+</div>
     <div class="container d-flex justify-content-center">
-       
-            <div class="col-lg-7 col-md-8 col-sm-10">
-                <div class="content-area">
-                    <?php the_content(); ?>
-                </div>
+
+        <div class="col-lg-8 col-md-8 col-sm-10">
+            <div class="content-area">
+                <?php the_content(); ?>
             </div>
-        
+        </div>
     </div>
+    
 </section>
 
 <section class="main-contents my-5">
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-6 col-6 mb-3">
-            <a href="#" class="card text-decoration-none text-dark text-center">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/tokio_bg001.png" alt="TOKIO INKARAMI">
-                <div class="card-body">
-                    <h3 class="m-0 fs-6 fw-600">TOKIO サロンシステム</h3>
-                </div>
-            </a>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-6 mb-3">
+                <a href="#" class="card text-decoration-none text-dark text-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/image/tokio_bg001.png" alt="TOKIO INKARAMI">
+                    <div class="card-body">
+                        <h3 class="m-0 fs-6 fw-600">TOKIO サロンシステム</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-6 mb-3">
+                <a href="#" class="card text-decoration-none text-dark text-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/image/tokio_bg002.png" alt="TOKIO INKARAMI">
+                    <div class="card-body">
+                        <h3 class="m-0 fs-6 fw-600">TOKIO IE ホームケア</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-6 mb-3">
+                <a href="#" class="card text-decoration-none text-dark text-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/image/tokio_bg003.png" alt="TOKIO INKARAMI">
+                    <div class="card-body">
+                        <h3 class="m-0 fs-6 fw-600">メカニズム</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-6 mb-3">
+                <a href="#" class="card text-decoration-none text-dark text-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/image/tokio-stars.png" alt="TOKIO INKARAMI">
+                    <div class="card-body">
+                        <h3 class="m-0 fs-6 fw-600">TOKIO STARS</h3>
+                    </div>
+                </a>
+            </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-6 mb-3">
-            <a href="#" class="card text-decoration-none text-dark text-center">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/tokio_bg002.png" alt="TOKIO INKARAMI">
-                <div class="card-body">
-                    <h3 class="m-0 fs-6 fw-600">TOKIO IE ホームケア</h3>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-6 mb-3">
-            <a href="#" class="card text-decoration-none text-dark text-center">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/tokio_bg003.png" alt="TOKIO INKARAMI">
-                <div class="card-body">
-                    <h3 class="m-0 fs-6 fw-600">メカニズム</h3>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-6 mb-3">
-            <a href="#" class="card text-decoration-none text-dark text-center">
-                <img src="<?php echo get_template_directory_uri(); ?>/image/tokio-stars.png" alt="TOKIO INKARAMI">
-                <div class="card-body">
-                    <h3 class="m-0 fs-6 fw-600">TOKIO STARS</h3>
-                </div>
-            </a>
-        </div>
-    </div>
 
-    
-</div>
+
+    </div>
 </section>
 
 
---
+++ page.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <header class="custom-page-header pt-5 pt-3">
+            <div class="container d-flex justify-content-center">
+            <div class="col-lg-6 col-md-8">
+                        <?php 
+                        
+                        if (has_post_thumbnail()) { ?>
+                            <img class="img-fluid" src="<?php echo get_the_post_thumbnail_url() ?>" alt="<? echo get_the_title() ?>">
+                        <?php } else { ?>
+                            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/image/placeholder.png" alt="<? echo get_the_title() ?>">
+                        <?php }
+
+                        ?>
+                    </div>
+            </div>
+            <div class="container d-flex justify-content-center">
+                    
+                    <div class="col-lg-10 col-md-10">
+                        <h1><?php echo get_the_title(); ?></h1>
+                        <p><?php echo get_the_content(); ?></p>
+                    </div>
+                
+            </div>
+        </header>
+
+    <?php endwhile;
+else : ?>
+    <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
